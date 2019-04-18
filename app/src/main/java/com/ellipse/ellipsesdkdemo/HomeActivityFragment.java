add039a869966251752db1c0202554ea53539802
Jignesh Tank <jignesh.tank@shreejisoftdevices.com>
2019-04-18 16:08:58 +0530

    Demo changes for handling different states of connections

--- app/src/main/java/com/ellipse/ellipsesdkdemo/HomeActivityFragment.java
+++ app/src/main/java/com/ellipse/ellipsesdkdemo/HomeActivityFragment.java
@@ -117,9 +117,6 @@ public class HomeActivityFragment extends Fragment {
 
         viewFlipper.setDisplayedChild(LAYOUT_CONNECT);
 
-        et_token.setText("2b0487a10bd4021bc83f47a0061b00892b9cd0b137edbc4ac0abc183e7dc8f7a");
-        et_ellipse_name_or_mac_id.setText("DB2959DF8354");
-
         tv_connect_lock.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
